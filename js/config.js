40b4301a72ed4a60c1819b7c58a64672cace2627
Shydow Lee <shydow@live.com>
2025-05-16 19:46:49 +0800

    Update site references and branding from LibreTV to 糖片TV across multiple files

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ const PASSWORD_CONFIG = {
 // 网站信息配置
 const SITE_CONFIG = {
     name: 'TangpianTV',
-    url: 'https://tangpian.com/video',
+    url: 'https://video.tangpian.com/',
     description: '免费在线视频搜索与观看平台',
     logo: './image/retrotv_5520.png',
     version: '1.0.3'
